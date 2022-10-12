50cdf70b2871335cf554b852e22fef430bbe5bec
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-12 15:17:05 -0300

    storybook

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,7 +6,16 @@ export default {
   component: Text,
   args: {
     children: 'Lorem Ipsum',
+    size: 'md',
   },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      }
+    }
+  }
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {};
@@ -29,5 +38,12 @@ export const CustomComponent: StoryObj<TextProps> = {
     children: (
       <p>Testando</p>
     )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    }
   }
 }
