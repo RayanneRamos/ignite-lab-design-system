3c7abc004c762935d7e7e4a9c8523c1ea387360a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-13 22:39:30 -0300

    Adicionando algumas flags nos arquivos do storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,6 +17,10 @@ module.exports = {
     "storyStoreV7": true,
     "interactionsDebugger": true,
   },
+  "staticDirs": [
+    "../public"
+  ],
+  
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-system'
