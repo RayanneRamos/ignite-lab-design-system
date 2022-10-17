dcfed5b20afd2a519dd93ce5ebd805af029905e1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 15:12:46 -0300

    Criando a página Register e o seu Storybook

---
+++ src/pages/Register/Register.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Register } from './Register';
+
+export default {
+  title: 'Pages/Register',
+  component: Register,
+  args: {},
+  argTypes: {},
+  parameters: {},
+} as Meta;
+
+export const Default: StoryObj = {};
