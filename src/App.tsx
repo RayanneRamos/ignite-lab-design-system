5837b92cbeb5f00affa3a6d4eae7ae6794e86d91
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 16:56:53 -0300

    Criando as rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-//import { ForgotPassword } from './pages/ForgotPassword/ForgotPassword';
-//import { SignIn } from './pages/SignIn/SignIn';
-//import { ChangePassword } from './pages/ChangePassword/ChangePassword';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ForgotPassword } from './pages/ForgotPassword/ForgotPassword';
+import { SignIn } from './pages/SignIn/SignIn';
+import { ChangePassword } from './pages/ChangePassword/ChangePassword';
 import { Register } from './pages/Register/Register';
 import './styles/global.css';
 
 function App() {
   return (
-    <Register />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/forgotpassword' element={<ForgotPassword />} />
+        <Route path='/changepassword' element={<ChangePassword />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
