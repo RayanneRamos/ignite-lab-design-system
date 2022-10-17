0e45ec821d5e2dbf7a546150af56e9c9589dada8
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 15:59:51 -0300

    Fazendo os testes na página ForgotPassword

--- src/pages/ForgotPassword/ForgotPassword.stories.tsx
+++ src/pages/ForgotPassword/ForgotPassword.stories.tsx
@@ -1,13 +1,35 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ForgotPassword } from './ForgotPassword';
-
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { rest } from 'msw';
 
 export default {
   title: 'Pages/Forgot password',
   component: ForgotPassword,
   args: {},
   argTypes: {},
-  parameters: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/forgotpassword', (req, res, ctx) => {
+          return res(ctx.json({
+            message: 'Link enviado com sucesso!',
+          }));
+        }),
+      ],
+    },
+  },
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    userEvent.type(canvas.getByPlaceholderText('Digite seu email cadastrado'), 'johndoe@example.com');
+    userEvent.type(canvas.getByPlaceholderText('*********'), '123456789');
+
+    await waitFor(() => {
+      expect(canvas.getByText('Link enviado com sucesso!')).toBeInTheDocument();
+    });
+  }
+};
