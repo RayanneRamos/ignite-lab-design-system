280acc70d99c8fd08f6305e5b509281530dbdd33
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 15:53:00 -0300

    Fazendo os testes na página ChangePassword

--- src/pages/ChangePassword/ChangePassword.stories.tsx
+++ src/pages/ChangePassword/ChangePassword.stories.tsx
@@ -1,12 +1,35 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ChangePassword } from './ChangePassword';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { rest } from 'msw';
 
 export default {
   title: 'Pages/ Change password',
   component: ChangePassword,
   args: {},
   argTypes: {},
-  parameters: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.put('/changepassword', (req, res, ctx) => {
+          return res(ctx.json({
+            massage: 'Senha mudada com sucesso!',
+          }));
+        }),
+      ],
+    },
+  },
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    userEvent.type(canvas.getByPlaceholderText('*********'), '123456789');
+    userEvent.click(canvas.getByRole('button'));
+    
+    await waitFor(() => {
+      expect(canvas.getByText('Senha mudada com sucesso!')).toBeInTheDocument();
+    });
+  }
+};
