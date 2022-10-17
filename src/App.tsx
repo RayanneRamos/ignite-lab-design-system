ee08be88614279bbfddc265117166646ee938a07
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 14:37:55 -0300

    Importando a página ForgotPassword

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { SignIn } from './pages/SignIn/SignIn';
+import { ForgotPassword } from './pages/ForgotPassword/ForgotPassword';
+//import { SignIn } from './pages/SignIn/SignIn';
 import './styles/global.css';
 
 function App() {
   return (
-    <SignIn />
+    <ForgotPassword />
   );
 }
 
