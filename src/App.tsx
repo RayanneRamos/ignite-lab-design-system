ab95eca8d27f4d31deca7967754ad3c21b34264f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-10-17 15:13:08 -0300

    Importando a página Register no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import { ForgotPassword } from './pages/ForgotPassword/ForgotPassword';
 //import { SignIn } from './pages/SignIn/SignIn';
-import { ChangePassword } from './pages/ChangePassword/ChangePassword';
+//import { ChangePassword } from './pages/ChangePassword/ChangePassword';
+import { Register } from './pages/Register/Register';
 import './styles/global.css';
 
 function App() {
   return (
-    <ChangePassword />
+    <Register />
   );
 }
 
